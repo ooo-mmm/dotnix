5caabfe84b079c74c07c378c0976ab42db892d3e
ooo-mmm <31175327+ooo-mmm@users.noreply.github.com>
2024-09-24 21:58:29 +0530

    Update autostart.sh

--- home/monk/config/awesome/awesome/config/autostart.sh
+++ home/monk/config/awesome/awesome/config/autostart.sh
@@ -10,5 +10,3 @@ run picom
 run xrdb -load "$HOME/.Xresources"
 run xwallpaper --stretch "$HOME/Pictures/Wallpapers/wallpaper.jpg"
 run setxkbmap -option caps:escape
-
-xrandr --output DP-0 --primary --mode 3440x1440 --pos 0x1080 --rotate normal --output DP-1 --off --output HDMI-0 --mode 2560x1080 --pos 383x0 --rotate normal --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output DP-4 --off --output DP-5 --off
