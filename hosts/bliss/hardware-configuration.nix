f25b43c14a7469768e006f76f48e53d856da3cc7
ooo-mmm <31175327+ooo-mmm@users.noreply.github.com>
2024-09-24 14:43:51 +0530

    Update hardware-configuration.nix

--- hosts/bliss/hardware-configuration.nix
+++ hosts/bliss/hardware-configuration.nix
@@ -9,35 +9,32 @@
     ];
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
-  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-label/boot";
+    { device = "/dev/disk/by-label/NIX_BOOT";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
     };
 
   fileSystems."/" =
-    { device = "/dev/disk/by-label/nixos";
+    { device = "/dev/disk/by-label/NIX_ROOT";
       fsType = "btrfs";
+      options = [ "subvol=@" ];
     };
 
-  fileSystems."/media/ssd" =
-    { device = "/dev/disk/by-label/ssd";
-      fsType = "btrfs";
+  fileSystems."/media/dev" =
+    { device = "/dev/disk/by-label/dev";
+      fsType = "ntfs";
     };
 
   fileSystems."/media/hdd" =
-    { device = "/dev/disk/by-label/hdd";
-      fsType = "ext4";
+    { device = "/dev/disk/by-label/data";
+      fsType = "ntfs";
     };
 
-  swapDevices = [ {
-    device = "/var/lib/swapfile";
-    size = 8*1024;
-  }];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
