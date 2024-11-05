e1d01db81c7348f47c18e12451254640a308b503
v <vaibhav7garg@gmail.com>
2024-11-05 06:45:22 +0530

    folder rename

--- home/default.nix
+++ home/default.nix
@@ -32,7 +32,7 @@
     };
     activation.installDots = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
       ${pkgs.rsync}/bin/rsync -avz --chmod=D2755,F744 ${
-        ./dotfiles/dots
+        ./dotfiles/hyprdots
       }/ ${config.xdg.configHome}/
     '';
   };
