2548a074b27d328b961cd7edaac579aef3d2f436
v <vaibhav7garg@gmail.com>
2025-04-01 21:50:48 +0530

    remove extra

--- hosts/os.nix
+++ hosts/os.nix
@@ -351,14 +351,6 @@ in {
     };
   };
 
-  # Virtualization / Containers
-  virtualisation.libvirtd.enable = false;
-  virtualisation.podman = {
-    enable = false;
-    dockerCompat = false;
-    defaultNetwork.settings.dns_enabled = false;
-  };
-
   # OpenGL
   hardware.graphics = { enable = true; };
 
